407e7144b4885693a46f73b58673fd1973ed87c3
nabehiro <v_hirow@sigmact.com>
2017-01-01 11:01:17 +0900

    add CsvRowReaderTest

--- TableIO.Tests/CsvRowReaderTests.cs
+++ TableIO.Tests/CsvRowReaderTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TableIO.Tests
 {
@@ -14,8 +15,48 @@ namespace TableIO.Tests
         [TestMethod()]
         public void ReadTest()
         {
-            // TODO:
-            Assert.Fail();
+            var sr = new StringReader("aaa,aaa,aaa\r\nbbb,bbb,bbb\r\n");
+            var reader = new CsvRowReader(sr);
+
+            CollectionAssert.AreEqual(new[] { "aaa", "aaa", "aaa" }, reader.Read().ToArray());
+            CollectionAssert.AreEqual(new[] { "bbb", "bbb", "bbb" }, reader.Read().ToArray());
+            Assert.IsNull(reader.Read());
         }
+
+        [TestMethod]
+        public void ReadNothingTest()
+        {
+            var sr = new StringReader("");
+            var reader = new CsvRowReader(sr);
+
+            Assert.IsNull(reader.Read());
+        }
+
+        [TestMethod]
+        public void ReadEmptyTest()
+        {
+            var sr = new StringReader(",,\n,,\r\n,");
+            var reader = new CsvRowReader(sr);
+
+            CollectionAssert.AreEqual(new[] { "", "", "" }, reader.Read().ToArray());
+            CollectionAssert.AreEqual(new[] { "", "", "" }, reader.Read().ToArray());
+            CollectionAssert.AreEqual(new[] { "", "" }, reader.Read().ToArray());
+            Assert.IsNull(reader.Read());
+        }
+
+        [TestMethod]
+        public void ReadEscapedTest()
+        {
+            var sr = new StringReader($@"""b"",""
+b"",""""""b"",""""""""
+""c,"","","",""{'\r'}"",""{'\n'}""
+");
+            var reader = new CsvRowReader(sr);
+
+            CollectionAssert.AreEqual(new[] { "b", "\r\nb", "\"b", "\"" }, reader.Read().ToArray());
+            CollectionAssert.AreEqual(new[] { "c,", ",", "\r", "\n" }, reader.Read().ToArray());
+            Assert.IsNull(reader.Read());
+        }
+
     }
 }
