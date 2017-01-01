8b1a6818c8df1f7f225c6b932d78b2179af1b98b
nabehiro <v_hirow@sigmact.com>
2017-01-01 13:00:10 +0900

    add InternalsVisibleTo

---
+++ TableIO.Tests/TableReaderTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TableIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableIO.Tests
+{
+    [TestClass()]
+    public class TableReaderTests
+    {
+        class Model
+        {
+
+        }
+
+        [TestMethod()]
+        public void ReadTest()
+        {
+            // TODO:
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConvertFromRowTest()
+        {
+            // TODO:
+            Assert.Fail();
+        }
+    }
+}
