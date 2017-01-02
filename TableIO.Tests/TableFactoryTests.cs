dac58bb58b7617504aa5c03e80941e601c73051c
nabehiro <v_hirow@sigmact.com>
2017-01-02 20:49:30 +0900

    add TableFactory.

---
+++ TableIO.Tests/TableFactoryTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TableIO.Tests
+{
+    [TestClass]
+    public class TableFactoryTests
+    {
+        class ValidCsvModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public string Remarks { get; set; }
+        }
+
+        [TestMethod]
+        public void ReadValidCsv()
+        {
+            using (var stmReader = new StreamReader("files\\Valid.csv"))
+            {
+                var csvReader = new TableFactory().CreateCsvReader<ValidCsvModel>(stmReader, true);
+                var models = csvReader.Read();
+
+                Assert.AreEqual(5, models.Count);
+
+                var model = models[3];
+                Assert.AreEqual(4, model.Id);
+                Assert.AreEqual("name \"4\"", model.Name);
+                Assert.AreEqual(4000, model.Price);
+                Assert.AreEqual("remarks4\nremarks4", model.Remarks);
+            }
+        }
+
+        [TestMethod]
+        public void CopyValidCsv()
+        {
+            IList<ValidCsvModel> models = null;
+            using (var stmReader = new StreamReader("files\\Valid.csv"))
+            {
+                var csvReader = new TableFactory().CreateCsvReader<ValidCsvModel>(stmReader, true);
+                models = csvReader.Read();
+            }
+
+            using (var stmWriter = new StreamWriter("files\\CopyValid.csv"))
+            {
+                var csvWriter = new TableFactory().CreateCsvWriter<ValidCsvModel>(stmWriter);
+                csvWriter.Write(models, new[] { "ID", "NAME", "PRICE", "REMARKS" });
+            }
+
+        }
+    }
+}
---
+++ TableIO/TableFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableIO
+{
+    public class TableFactory
+    {
+        public TableReader<TModel> CreateCsvReader<TModel>(TextReader textReader,
+            bool hasHeader = false,
+            ITypeConverterResolver typeConverterResolver = null,
+            IPropertyMapper propertyMapper = null,
+            IModelValidator modelValidator = null)
+            where TModel : new()
+        {
+            var rowReader = new CsvRowReader(textReader);
+            typeConverterResolver = typeConverterResolver ?? new DefaultTypeConverterResolver<TModel>();
+            propertyMapper = propertyMapper ?? new AutoIndexPropertyMapper();
+            modelValidator = modelValidator ?? new NullModelValidator();
+
+            var reader = new TableReader<TModel>(rowReader, typeConverterResolver, propertyMapper, modelValidator);
+            reader.HasHeader = hasHeader;
+
+            return reader;
+        }
+
+        public TableWriter<TModel> CreateCsvWriter<TModel>(TextWriter textWriter,
+            ITypeConverterResolver typeConverterResolver = null,
+            IPropertyMapper propertyMapper = null)
+        {
+            var rowReader = new CsvRowWriter(textWriter);
+            typeConverterResolver = typeConverterResolver ?? new DefaultTypeConverterResolver<TModel>();
+            propertyMapper = propertyMapper ?? new AutoIndexPropertyMapper();
+
+            return new TableWriter<TModel>(rowReader, typeConverterResolver, propertyMapper);
+        }
+    }
+}
